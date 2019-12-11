6f7a43cc0b7f5e4b4667b8aab570dfcd61245a4d
zhuowp <zhuowp@outlook.com>
2019-12-11 13:52:23 +0800

    日志

---
+++ SMS.Client.Log/LogHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SMS.Client.Log
+{
+    public class LogHelper : ILog
+    {
+        public void Debug(object message)
+        {
+        }
+
+        public void DebugFormatted(string format, params object[] args)
+        {
+        }
+
+        public void Error(object message)
+        {
+        }
+
+        public void Error(object message, Exception exception)
+        {
+        }
+
+        public void ErrorFormatted(string format, params object[] args)
+        {
+        }
+
+        public void Fatal(object message)
+        {
+        }
+
+        public void Fatal(object message, Exception exception)
+        {
+        }
+
+        public void FatalFormatted(string format, params object[] args)
+        {
+        }
+
+        public void Info(object message)
+        {
+        }
+
+        public void InfoFormatted(string format, params object[] args)
+        {
+        }
+
+        public void Warn(object message)
+        {
+        }
+
+        public void Warn(object message, Exception exception)
+        {
+        }
+
+        public void WarnFormatted(string format, params object[] args)
+        {
+        }
+    }
+}
